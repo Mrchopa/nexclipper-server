6570d1d453e42f18ee3bde70cbd18f785efebbc7
cjh <paul911014@naver.com>
2020-07-14 17:51:11 +0900

    create project

---
+++ src/main/java/co/kr/nexclipper/nexclipperserver/NexclipperServerApplication.java
@@ -0,0 +1,13 @@
+package co.kr.nexclipper.nexclipperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexclipperServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexclipperServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/nexclipper/nexclipperserver/NexclipperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.nexclipper.nexclipperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexclipperServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
